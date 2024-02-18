c8a89fcc44c2a420c51739fa4ab30d3f6d049bb8
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-02-18 08:38:38 -0800

    [ADDED]:shubham Welcome Message of Employee Wage computation

---
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -0,0 +1,11 @@
+package com.shubham.EmployeeWagecomp.java;
+
+
+public class EmployeeWagecomp {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Welcome to Employee Wage Compulatiopn");
+	}
+}
