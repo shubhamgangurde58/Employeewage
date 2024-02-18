1c189fad45f5298cd301793317375dbea0bc54ae
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-02-18 08:58:36 -0800

    [MODIFIED]:shubham UC-1- for Employee Attendance

--- src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -1,11 +1,23 @@
 package com.shubham.EmployeeWagecomp.java;
 
+import java.util.Random;
 
 public class EmployeeWagecomp {
 
 	public static void main(String[] args) {
-
 		
+		int attendance = 0 ;
 		System.out.println("Welcome to Employee Wage Compulatiopn");
+		
+		Random random = new Random();
+		attendance = random.nextInt(9) % 2;
+		if(attendance == 0)
+		{
+			System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println("Employee is Present");
+		}
 	}
 }
