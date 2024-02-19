97b8121c4ec471149bb001b1339dadb329742a05
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-02-18 20:07:31 -0800

    [MODIFIED]:shubham Refactored To Switch Case]

--- src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -16,28 +16,28 @@ public class EmployeeWagecomp {
 		
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2;
-		if(attendance == 0)
+		switch(attendance)
 		{
+		case 0:
 			System.out.println("Employee is Absent");
-		}
-		else
-		{
+			break;
+		case 1:
 			employee_type = random.nextInt(9) % 2;
-			if(employee_type == 0)
-			{
-				System.out.println("Employee is partTime");
-				dailywage = PART_TIME_WAGE * WAGE_PER_HR;
+				switch(employee_type)
+				{
+					case 0:
+						System.out.println("Employee is partTime");
+						dailywage = PART_TIME_WAGE * WAGE_PER_HR;
+					    break;
+					case 1:
+						System.out.println("Employee is fullTime");
+						dailywage = FULL_TIME_WAGE * WAGE_PER_HR;
+						break;
+				}
 
-			}
-			else
-			{
-				System.out.println("Employee is fullTime");
-				dailywage = FULL_TIME_WAGE * WAGE_PER_HR;
-			}
-
-			System.out.println("DailyWage="+dailywage);
+				System.out.println("DailyWage="+dailywage);
+				break;
 		}
-			
 	}
 
 }		
